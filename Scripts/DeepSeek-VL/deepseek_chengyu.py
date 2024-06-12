8d53f16342f0a0a5fb63c74adb98686881be3296
Eternity-gaga <72152902+Eternity-gaga@users.noreply.github.com>
2024-06-12 21:02:54 +0800

    Add files via upload

---
+++ Scripts/DeepSeek-VL/deepseek_chengyu.py
@@ -0,0 +1,55 @@
+random.seed(2024)
+
+import torch
+from transformers import AutoModelForCausalLM
+
+from deepseek_vl.models import VLChatProcessor, MultiModalityCausalLM
+from deepseek_vl.utils.io import load_pil_images
+
+
+# specify the path to the model
+model_path = "./deepseek-vl-7b-chat"
+vl_chat_processor: VLChatProcessor = VLChatProcessor.from_pretrained(model_path)
+tokenizer = vl_chat_processor.tokenizer
+
+vl_gpt: MultiModalityCausalLM = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True)
+vl_gpt = vl_gpt.to(torch.bfloat16).cuda().eval()
+
+conversation = [
+    {
+        "role": "User",
+        "content": "<prompt_template>",
+        "images": ["file_path"]
+    },
+    {
+        "role": "Assistant",
+        "content": ""
+    }
+]
+
+# load images and prepare for inputs
+pil_images = load_pil_images(conversation)
+prepare_inputs = vl_chat_processor(
+    conversations=conversation,
+    images=pil_images,
+    force_batchify=True
+).to(vl_gpt.device)
+
+# run image encoder to get the image embeddings
+inputs_embeds = vl_gpt.prepare_inputs_embeds(**prepare_inputs)
+
+# run the model to get the response
+with torch.no_grad():
+    outputs = vl_gpt.language_model.generate(
+        inputs_embeds=inputs_embeds,
+        attention_mask=prepare_inputs.attention_mask,
+        pad_token_id=tokenizer.eos_token_id,
+        bos_token_id=tokenizer.bos_token_id,
+        eos_token_id=tokenizer.eos_token_id,
+        max_new_tokens=256,
+        do_sample=False,
+        use_cache=True
+    )
+
+answer = tokenizer.decode(outputs[0].cpu().tolist(), skip_special_tokens=True)
+print(f"{prepare_inputs['sft_format'][0]}", answer)
